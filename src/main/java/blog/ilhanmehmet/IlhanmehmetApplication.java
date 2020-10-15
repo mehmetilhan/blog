3f7d5ae78ae68133ed832c280a8af40e598fa434
mehmet ilhan <mehmet.ilhan@titck.gov.tr>
2020-10-16 02:54:22 +0300

    no message

---
+++ src/main/java/blog/ilhanmehmet/IlhanmehmetApplication.java
@@ -0,0 +1,13 @@
+package blog.ilhanmehmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlhanmehmetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlhanmehmetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blog/ilhanmehmet/IlhanmehmetApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.ilhanmehmet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlhanmehmetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
